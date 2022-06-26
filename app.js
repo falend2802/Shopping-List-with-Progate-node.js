ec6f9f1de3fd23d5a2b94862ea28f05a8892397b
Moh Falend Wandamen <62651106+falendwa@users.noreply.github.com>
2022-06-27 00:08:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.post('/create', (req, res) => {
    });
 });
 
-app.post('/delete', (req, res) => {
+app.post('/delete/:id', (req, res) => {
+  console.log(req.params.id);
   res.redirect('/index');
 });
 
